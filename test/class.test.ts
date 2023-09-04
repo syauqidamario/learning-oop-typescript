bca19283e046aa3505a817875ebd771f5b5c0bed
syauqidamario <syauqi.djohan@student.umn.ac.id>
2023-09-04 19:32:08 +0700

    first commit

---
+++ test/class.test.ts
@@ -0,0 +1,14 @@
+describe('Class', ()=> {
+      it('should can create class', () => {
+            class Customer {
+
+            }
+
+            class Order {
+
+            }
+
+            const customer: Customer = new Customer();
+            const order = new Order();
+      });
+});
